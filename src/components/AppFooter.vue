a0a874323e067c2c863b44b0a48092820c8a1378
AlBandid <aleksandrakoziol77@gmail.com>
2025-10-21 21:48:26 +0200

    added privacy policy
    privacy policy is necessary for google developer console

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -5,8 +5,13 @@ export default {
 </script>
 
 <template>
-<div class="flex flex-col sm:flex-row inset-x-0 bottom-0 w-full h-20 xs:h-10 sm:h-5 p-5 place-content-center place-items-center text-center sm:place-content-between bg-fuchsia-100/50 text-sm sm:text-base text-fuchsia-900 drop-shadow-lg">
-  <p class="">© PTPiPN 2025</p>
+<div class="flex flex-col sm:flex-row inset-x-0 bottom-0 w-full h-30 xs:h-20 sm:h-15 lg:h-5 p-5 place-content-center place-items-center text-center sm:place-content-between bg-fuchsia-100/50 text-sm sm:text-base text-fuchsia-900 drop-shadow-lg">
+  <div class="flex flex-col sm:flex-row">
+    <p class="">© PTPiPN 2025</p>
+    <p class="">
+      <a href="/docs/Polityka_prywatnosci.pdf" target="_blank" rel="noopener noreferrer">Polityka prywatności</a>
+    </p>
+  </div>
   <p class="">Projekt i wykonanie strony internetowej: <a href="mailto:a.koziol-wasik@outlook.com">Aleksandra Kozioł-Wąsik</a></p>
 </div>
 </template>
